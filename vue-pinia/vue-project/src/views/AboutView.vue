f76caaf5aab64358726c0b8d1336ac367b60e88e
haejujung <167273792+haejujung@users.noreply.github.com>
2026-01-30 20:17:26 +0900

    pinia

---
+++ vue-pinia/vue-project/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useCountOptionStore} from '@/stores/countOption';
+import { useCountSetupStore} from '@/stores/countSetup';
+import { storeToRefs} from 'pinia'; // 비구조화 할당
+const optionStore = useCountOptionStore();
+const setupStore = useCountSetupStore();
+
+const {num, doubleNum} = storeToRefs(optionStore); // setupStore 도 가능 
+
+console.log(optionStore.num);
+console.log(setupStore.doubleNumnum);
+optionStore.increment();
+setupStore.getJSON('https://jsonplaceholder.typicode.com/posts');
+console.log(num, doubleNum);
+
+</script>
+
+<template>
+  <h1>About View</h1>
+</template>
